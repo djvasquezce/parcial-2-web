0a6827d7f9cd957f223d14a6bc36fe9e03d618c0
Danny <djvasquezce@unal.edu.co>
2022-06-22 13:05:11 -0500

    Parcial + base de datos  + tabla países específica

---
+++ tablaPaises.sql
@@ -0,0 +1,77 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 22-06-2022 a las 20:03:25
+-- Versión del servidor: 10.4.24-MariaDB
+-- Versión de PHP: 7.4.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `bd-danny`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `paises`
+--
+
+CREATE TABLE `paises` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `nombre` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `capital` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `idioma` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `poblacion` bigint(20) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `paises`
+--
+
+INSERT INTO `paises` (`id`, `nombre`, `capital`, `idioma`, `poblacion`, `created_at`, `updated_at`) VALUES
+(1, 'Colombia', 'Bogotá', 'Español', 123, NULL, NULL),
+(2, 'Perú', 'Lima', 'Español', 123123, NULL, NULL),
+(3, 'Argentina', 'Buenos Aires', 'Español', 31231, NULL, NULL),
+(4, 'Venezuela', 'Caracas', 'Español', 131213, NULL, NULL),
+(5, 'Brasil', 'Brasilia', 'Portugues', 12313, NULL, NULL),
+(6, 'Chile', 'Santiago de Chile', 'Español', 13131, NULL, NULL),
+(8, 'Ecuador', 'Quito', 'Español', 3123131, NULL, NULL),
+(10, 'Costa Rica', 'San José', 'Español', 313213, NULL, NULL);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `paises`
+--
+ALTER TABLE `paises`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `paises`
+--
+ALTER TABLE `paises`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
